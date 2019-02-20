d6aaa78c8e629ecd929535cda680aef08e6f6a62
wang <767074895@qq.com>
2019-02-20 18:02:37 +0800

    ''

--- src/train/pages/app.js
+++ src/train/pages/app.js
@@ -4,21 +4,28 @@
  * @date: 2018-10-08
  */
 
-import React, { Component } from "react";
+import React, { Component,useState, useEffect } from "react";
 import Nav from '../../common/component/nav';
 import  aboutpic from '../images/about.jpg'
 import {Link}  from 'react-router-dom';
 
-export default class App extends Component {
-  render() {
+import { useSpring, animated,path, Donut} from 'react-spring'
+
+export default function app(){
+
+
+    const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+    const propsLine = useSpring({ number: 1, from: { number: 0 } });
   	return (
   		<div>
   			<Nav />
   			<div className="about">
   				<img src={aboutpic} />
                 <Link to='/sec'><p>and this is about page</p></Link>
-        </div>
+                <animated.div style={props}>I will fade in</animated.div>
+                <animated.span>{propsLine.number}</animated.span>
+
+            </div>
       </div>
     );
-  }
 }
